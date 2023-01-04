fd8ac17a3a4ba5eca507e4db22686d9785e7307b
ZaharKoshin <z.koshin@itransition.con>
2023-01-04 18:41:22 +0300

    ref

--- P2PConnect.Service/Models/CommandActionsModel.cs
+++ P2PConnect.Service/Models/CommandActionsModel.cs
@@ -10,37 +10,12 @@ namespace P2PConnect.Service.Models
 {
     public class CommandActionsModel
     {
-        public CommandActionsModel(UdpReceiveResult? receiveResult, int? port)
-        {
-            this.receiveResult = receiveResult;
-            this.port = port;
-        }
-
-        public CommandActionsModel(object? obj)
-        {
-            this.obj = obj;
-        }
-
-        public CommandActionsModel(string? message)
-        {
-            this.message = message;
-        }
-
-        public CommandActionsModel(UdpReceiveResult? receiveResult, object? obj, string? message)
-        {
-            this.receiveResult = receiveResult;
-            this.obj = obj;
-            this.message = message;
-        }
-
-        public UdpReceiveResult? receiveResult { get; set; }
-
-        public IPEndPoint IPEndPoint { get; set; }
-
         public CommandActionsModel(IPEndPoint iPEndPoint)
         {
             IPEndPoint = iPEndPoint;
         }
+
+        public IPEndPoint IPEndPoint { get; set; }
 
         public object? obj { get; set; }
 
--- P2PConnectClient/ProcessClient.cs
+++ P2PConnectClient/ProcessClient.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using P2PConnect.Service.Interfaces;
+using System;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace P2PConnectClient
 {
     public class ProcessClient : IProcessClient
     {
+        private readonly IReceiveService _receiveService;
+
+        public ProcessClient(IReceiveService receiveService)
+        {
+            _receiveService = receiveService;
+        }
+
         public async Task ProcessAsync()
         {
-            Console.WriteLine("Start Client");
+            var startClient = true;
+            while (startClient)
+            {
+                var result = await _receiveService.ReceiveAsync();
+            }
         }
     }
 }
